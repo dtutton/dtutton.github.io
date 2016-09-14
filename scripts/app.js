f6571da62a56228f62dc6e048f25882b4f132515
DAN TUTTON <DAN TUTTON>
2016-09-14 15:54:46 -0700

    Test 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,21 @@
-function buttonOneClicked() {
-    alert("You clicked button 1");
-}
+showContent("home.html");
+
 
 function Button2Clicked() {
     document.body.style.backgroundColor = "powderblue"; 
 }
+
+function placeMarker(map, location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+  });
+}
+
+function functionAlert(){
+    alert('menu clicked and called function');
+};
+
+function showContent(pageName){
+    $('#contentDiv').load(pageName);
+};
